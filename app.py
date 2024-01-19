0b2003c2ae64695941bb3e26cd8700f2787e8193
CaueFoyth <foythcaue@gmail.com>
2024-01-18 21:37:49 -0300

    Aula 2

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-print("Sabor Express\n")
+import os
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair")
+def exibir_nome_prgrama(): 
+    print("Sabor Express\n")
 
-opcao = input("\nEscolha uma opção: ")
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair")
+
+def finaliza_app():
+    os.system("cls")
+    print("Encerrando o programa...\n")
+
+def escolher_opcao():
+    opcao = int(input('Escolha uma opção: '))
+    match opcao:
+        case 1:
+            print('Adicionar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finaliza_app()
+        case _:
+            print('Opção inválida!')
+
+
+def main():
+    exibir_nome_prgrama()
+    exibir_opcoes()
+    escolher_opcao()
+
+# Arquivo principal do prgrama
+if __name__ == '__main__':
+    main()
