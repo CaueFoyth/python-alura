bec16310da30b11f4f35f5aa64abd24a78e876dd
CaueFoyth <foythcaue@gmail.com>
2024-02-08 21:02:15 -0300

    Finalização

--- aprendendo-api/main.py
+++ aprendendo-api/main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
 
 app = FastAPI()
 
 @app.get('/api/hello')
 def hello_word():
+    '''
+    Endpoint que exibi uma mensagem incrivel!
+    '''
     return {'Hello':'world'}
+
+@app.get('/api/restaurantes/')
+def get_restaurantes(restaurante: str = Query(None)):
+
+    '''
+    Endpoint para ver o cardápio dos restaurantes
+    '''
+
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados': dados_json}
+        
+        dados_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:  
+                dados_restaurante.append({
+                    "item":item['Item'],
+                    "price":item['price'],
+                    "description":item['description']
+                })
+        
+        return {'Restaurante':restaurante, 'Cardapio':dados_restaurante}
+    else:
+        return{f'Erro':f'{response.status_code} - {response.text}'}
